dd9317547191ab250842bced9657f758cc1adf36
Sofi Cherif <soficherif@me.com>
2023-01-02 12:32:05 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import Youtube from './Youtube';
+import OldMan  from './OldMan';
+import Buckets from './Buckets';
+import Kevin from './Kevin'
+
+
+
+
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+        <OldMan/>
+        <Kevin/>
+        <Buckets/>
+        <Youtube/>
+        
+        
       </header>
+      
     </div>
   );
 }
---
+++ src/Youtube.js
@@ -0,0 +1,75 @@
+import React, {Component} from 'react';
+
+
+const API_KEY = 'AIzaSyBv5E6MA2ejzzfObBSZcKU7wz4e4AXNtAg'
+
+
+var finalURL = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=UC2ozVs4pg2K3uFLw6-0ayCQ&maxResults=20&order=date&key=${API_KEY}`
+
+
+class Youtube extends Component {
+    
+    constructor(props){
+        super(props);
+
+        this.state = {
+            resultyt: []
+
+        };
+
+        this.clicked = this.clicked.bind(this);
+
+        
+    }
+
+    clicked() {
+        fetch(finalURL)
+        .then((response) => response.json())
+        .then((responseJson)=> {
+           //console.log(responseJson); 
+           const resultyt = responseJson.items.map(obj => "https://www.youtube.com/embed/"+obj.id.videoId);
+           this.setState({resultyt});
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+    }
+
+   
+    
+   
+    
+    
+    render() {
+        console.log(finalURL);
+        console.log(this.state.resultyt)
+
+        return(
+            <div className="rowstyle">
+                <button className="button" onClick={this.clicked}>Shotime Basketball Videos</button>
+           
+                {
+                    this.state.resultyt.map((link, i) => {
+                            console.log(link);
+                            var frame = <div  key={i} className="youtube"> <iframe width="560" height="315" src={link} frameBorder="0" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe> </div>
+                            return frame;
+                    })
+                  }  
+
+                  {this.frame}
+           
+           
+           
+           
+                
+                    
+
+
+            
+            </div>
+
+        );
+    }
+ }
+
+ export default Youtube; 
